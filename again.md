815fc55d33e9619d204118c2f88fa5541eb57bc5
yangahh <toaur6802@naver.com>
2021-04-28 04:06:22 +0900

    Add: admin.md file

---
+++ again.md
@@ -0,0 +1,4 @@
+## 알고리즘 스터디 후 다시 풀 문제
+
+- week02 - day05 (이진탐색)
+- week03 - day05 (factorial)
--- codekata15.py
+++
@@ -1,9 +0,0 @@
-# 팩토리얼
-def factorial(n):
-    if n <= 1:
-        return 1
-
-    return n * factorial(n-1)
-
-
-print(factorial(5))
